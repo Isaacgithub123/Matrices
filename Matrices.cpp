2f9cd7c9b5bd5ec481f664a7b2d4bfed99fe4491
Isaacgithub123 <isaacazofeifab@gmail.com>
2026-05-15 16:35:35 -0600

    Nuevo Main

--- Matrices.cpp
+++ Matrices.cpp
@@ -4,41 +4,12 @@
 using namespace std;
 
 int main() {
-    Matrix<int> m(2, 2);
+    int filas, columnas;
 
-    // Asignar valores
-    m.setValue(0, 0, 1);
-    m.setValue(0, 1, 2);
-    m.setValue(1, 0, 3);
-    m.setValue(1, 1, 4);
+    cout << "Ingrese cantidad de filas: ";
+    cin >> filas;
 
-    cout << "Matriz original:" << endl;
-    m.print();
+    cout << "Ingrese cantidad de columnas: ";
+    cin >> columnas;
 
-    // Agregar fila
-    m.addRow(9);
-    cout << "\nDespues de agregar fila:" << endl;
-    m.print();
-
-    // Agregar columna
-    m.addColumn(7);
-    cout << "\nDespues de agregar columna:" << endl;
-    m.print();
-
-    // Transponer
-    m.transpose();
-    cout << "\nDespues de transpose:" << endl;
-    m.print();
-
-    // Eliminar fila
-    m.removeRow(0);
-    cout << "\nDespues de eliminar fila 0:" << endl;
-    m.print();
-
-    // Eliminar columna
-    m.removeColumn(0);
-    cout << "\nDespues de eliminar columna 0:" << endl;
-    m.print();
-
-    return 0;
-}
+    Matrix<int> matriz(filas, columnas);
