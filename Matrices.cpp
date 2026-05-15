b91c670e8af1131fc67d23334b357b5a9425eb53
Isaacgithub123 <isaacazofeifab@gmail.com>
2026-05-15 16:29:42 -0600

    Im[lementacion final de Matrix con main de prueba

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,20 +1,44 @@
-// Matrices.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include "Matrix.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    Matrix<int> m(2, 2);
+
+    // Asignar valores
+    m.setValue(0, 0, 1);
+    m.setValue(0, 1, 2);
+    m.setValue(1, 0, 3);
+    m.setValue(1, 1, 4);
+
+    cout << "Matriz original:" << endl;
+    m.print();
+
+    // Agregar fila
+    m.addRow(9);
+    cout << "\nDespues de agregar fila:" << endl;
+    m.print();
+
+    // Agregar columna
+    m.addColumn(7);
+    cout << "\nDespues de agregar columna:" << endl;
+    m.print();
+
+    // Transponer
+    m.transpose();
+    cout << "\nDespues de transpose:" << endl;
+    m.print();
+
+    // Eliminar fila
+    m.removeRow(0);
+    cout << "\nDespues de eliminar fila 0:" << endl;
+    m.print();
+
+    // Eliminar columna
+    m.removeColumn(0);
+    cout << "\nDespues de eliminar columna 0:" << endl;
+    m.print();
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
