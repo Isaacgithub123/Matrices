4675dc1fe4073e431f7c6cf668d44ccccf5e67d6
Isaacgithub123 <isaacazofeifab@gmail.com>
2026-05-15 16:49:44 -0600

    Implementacion de #include <cstdlib> #include <ctime>   para usar la hora para generar numeros random

--- Matrices.cpp
+++ Matrices.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "Matrix.h"
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main() {
     int filas, columnas;
-
     cout << "Ingrese cantidad de filas: ";
     cin >> filas;
 
@@ -13,3 +14,13 @@ int main() {
     cin >> columnas;
 
     Matrix<int> matriz(filas, columnas);
+    return 0;
+    
+    rand(time(0));
+
+    int opcion = -1;
+
+    while (opcion != 0) {
+        matriz->print();
+
+}
